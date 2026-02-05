5aa0b7ab94ce277ba10fcfb85d51851b176714c6
mathanponsingh <154612384+mathanponsingh@users.noreply.github.com>
2026-02-05 16:38:54 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateToken = (id, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
