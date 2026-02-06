d53634813dfcab812a3bae847d4aee23fad50165
mathanponsingh <154612384+mathanponsingh@users.noreply.github.com>
2026-02-06 20:39:05 +0530

    Update Employee.jsx

--- admin/src/Pages/Employee.jsx
+++ admin/src/Pages/Employee.jsx
@@ -78,7 +78,7 @@ const EmployeesPage = () => {
 
     <button
       onClick={() => setIsModalOpen(true)}
-      className="bg-blue-600 text-white px-4 py-2 rounded-md 
+      className="bg-blue-600 cursor-pointer px-4 py-2 rounded-md 
                  hover:bg-blue-700 transition whitespace-nowrap"
     >
       + Add Employee
